03da0e4e15cbbb0444070a89cffc81da6512c55e
Roberto Javier Albanesi <beto0607@gmail.com>
2019-02-04 12:18:30 -0300

    cleaned user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
   before_action :authenticate_user, only: [:show, :index]
 
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
-  # # GET /users/1
-  # def show
-  #   render json: @user
-  # end
-
-  # POST /users
+  #POST /users
   def create
     @user = User.new(user_params)
 
@@ -25,23 +13,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # def update
-  #   if @user.update(user_params)
-  #     render json: @user
-  #   else
-  #     render json: @user.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # # DELETE /users/1
-  # def destroy
-  #   @user.destroy
-  # end
-  
-  # def user_url arg
-  #   users_url arg
-  # end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
