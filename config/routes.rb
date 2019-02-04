9ed4a80c48632992a156d2c5b5989d054b2db6c8
Roberto Javier Albanesi <beto0607@gmail.com>
2019-02-04 12:17:10 -0300

    added test for answers. Cleaned routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-  #  post 'user_token' => 'user_token#create'
-  
   post '/sessions' => 'user_token#create' #For login
-  # resources :users, only: ['create', 'index'] #For signup
-  # #resources :questions, only:['index', 'show', 'create', 'update', 'detroy']
-  # resources :questions, only:['index', 'show', 'create', 'update', 'detroy'] do
-  #   put '/resolve', to: 'question#resolve'
-  #   resources :answers, only: ['index', 'destroy', 'create']
-  # end
-  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
-  jsonapi_resources :questions
-  jsonapi_resources :answers
+  jsonapi_resources :questions, only:['index', 'show', 'create', 'update', 'detroy'] do
+    jsonapi_resources :answers, only: ['index', 'destroy', 'create']do
+    end
+    put '/resolve', to: 'question#resolve'
+  end
 end
--- test/models/answer_test.rb
+++ test/models/answer_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class AnswerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.create(
+      username: "answerOwner",
+      email: "answerOwner@answer.com",
+      screen_name: "Answer Owner",
+      password: "pass",
+      password_confirmation: "pass"
+    )
+    @question = @user.questions.create(
+      title: "Test question",
+      description: "Test question"
+    )
+    @answer = Answer.create(
+      user_id: @user.id,
+      question_id: @question.id,
+      content: "Answer content"
+    )
+  end
+
+  test "Create answer" do
+    a = Answer.create(
+      user_id: @user.id,
+      question_id: @question.id,
+      content: "Answer content"
+    )
+    assert a.persisted?
+  end
+
+  test "Cannot create answer without all parameter" do
+    a = Answer.create(
+      user_id: @user.id,
+      question_id: @question.id
+      #No content
+    )
+    assert_not a.persisted?
+    a = Answer.create(
+      question_id: @question.id,
+      content: "No user"
+    )
+    assert_not a.persisted?
+    a = Answer.create(
+      user_id: @user.id,
+      content: "No question"
+    )
+    assert_not a.persisted?
+  end
 end
